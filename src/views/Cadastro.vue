4e8051c56f1c86014b9b117bd75b2ef0fa35d3ae
Anderson Kardek <kardek.anderson@gmail.com>
2021-07-13 07:56:34 -0400

    npm i

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     <h1>CADASTRO</h1>
+    <h2>sou eu</h2>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'cadastro'
+  name: 'cadastro',
+  
+  data:() => ({
+    x:0,
+    lista: []
+  }),
+
+  methods: {
+    soma (){
+      console.log('oi')
+    }
+  }
 }
 </script>
 
